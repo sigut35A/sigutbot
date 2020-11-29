c366be984bbec44dd2d087b90d6ceea4b3ccb31d
sigut35A <75209430+sigut35A@users.noreply.github.com>
2020-11-29 22:38:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const BotName = 'sigutbot☄'; 
+const instagram = 'https://instagram.com/sigitdwinugroho30'; 
+const telegram = 'https://t.me/masihrahasia'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const youtube = 'https://www.youtube.com/saeesport';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
